c058d04300641661df9df46e71da75876fe65ff3
david <dkim187@binghamton.edu>
2020-06-15 21:45:06 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,26 @@
+
+
+
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop
+  array.pop
+end
+
+def pop_with_args
+  array.pop
+end
+
+def using_shift
+  array.shift
+end
+
+def shift_with_args(2)
+  array.shift(2)
+end
